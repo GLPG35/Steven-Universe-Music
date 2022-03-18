eda474922053a4a0cf3b3f0c0611834549c12380
UNIT Enterprise Systems <55563130+GLPG35@users.noreply.github.com>
2022-03-18 11:26:42 -0300

    <3

--- js/script.js
+++ js/script.js
@@ -38,3 +38,22 @@ particlesJS (
         }
     }
 );
+
+$('.al').mouseenter(function() {
+    let albumName = this.className;
+    let first = albumName.split(' ');
+    let i = '.' + first[0];
+
+    $(i + ' .buttons .btn1 a').mouseenter(function() {
+        $(':root').css('--color', '#3a8b8b');
+        $(':root').css('--album_1', '150% at 25% 50%');
+    }).mouseleave(function() {
+        $(':root').css('--album_1', '0% at 25% 50%');
+    });
+    $(i + ' .buttons .btn2 a').mouseenter(function() {
+        $(':root').css('--color', '#823a8b');
+        $(':root').css('--album_1', '150% at 75% 50%');
+    }).mouseleave(function() {
+        $(':root').css('--album_1', '0% at 75% 50%');
+    });
+});
