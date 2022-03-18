b7ceda0df88efa8c578b2cd0bfeabf79892ca57f
UNIT Enterprise Systems <55563130+GLPG35@users.noreply.github.com>
2022-03-17 23:28:49 -0300

    <3

---
+++ js/script.js
@@ -0,0 +1,40 @@
+particlesJS (
+    "particles",
+
+    {
+        particles: {
+            number: {
+                value: 50,
+                density: {
+                    enable: true,
+                    value_area: 1000
+                }
+            },
+            color: {
+                value: "#f0f0f0"
+            },
+            shape: {
+                type: "star"
+            },
+            size: {
+                value: 7,
+                random: true
+            },
+            move: {
+                enable: true,
+                speed: 1,
+                direction: "bottom-left"
+            },
+            line_linked: {
+                enable: false
+            }
+        },
+        interactivity: {
+            events: {
+                onhover: {
+                    enable: false
+                }
+            }
+        }
+    }
+);
