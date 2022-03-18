ce2a47cf16691b1034759f36b0f0a1ac0ee2046f
UNIT Enterprise Systems <55563130+GLPG35@users.noreply.github.com>
2022-03-18 11:51:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,17 +43,29 @@ $('.al').mouseenter(function() {
     let albumName = this.className;
     let first = albumName.split(' ');
     let i = '.' + first[0];
+    let n = 0;
 
     $(i + ' .buttons .btn1 a').mouseenter(function() {
+        n = 1;
         $(':root').css('--color', '#3a8b8b');
         $(':root').css('--album_1', '150% at 25% 50%');
-    }).mouseleave(function() {
-        $(':root').css('--album_1', '0% at 25% 50%');
     });
+    
     $(i + ' .buttons .btn2 a').mouseenter(function() {
+        n = 2;
         $(':root').css('--color', '#823a8b');
         $(':root').css('--album_1', '150% at 75% 50%');
-    }).mouseleave(function() {
-        $(':root').css('--album_1', '0% at 75% 50%');
+    });
+
+    $(i + ' .buttons').mouseleave(function() {
+        switch (n) {
+            case 1:
+                $(':root').css('--album_1', '0% at 25% 50%');
+                break;
+            
+            case 2:
+                $(':root').css('--album_1', '0% at 75% 50%');
+                break;
+        }
     });
 });
