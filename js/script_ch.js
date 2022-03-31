d10b37ec2180274e62cb6628e105620b63e29d6b
UNIT Enterprise Systems <55563130+GLPG35@users.noreply.github.com>
2022-03-30 21:07:55 -0300

    v6.0
    Added 'Characters' section!

--- js/script_ch.js
+++ js/script_ch.js
@@ -81,12 +81,13 @@ $.get('./js/characters.txt', function(data) {
     let lines = data.split('\n');
 
     lines.forEach(function(n) {
-        let split = n.split(', ');
+        let split = n.split('; ');
         let element = {};
 
         element.character = split[0];
         element.description = split[1];
         element.icon = split[2];
+        element.bg = split[3];
 
         characterList.push(element);        
     });
@@ -94,7 +95,11 @@ $.get('./js/characters.txt', function(data) {
     characterList.forEach(function(n) {
         let classN = n.character.toLowerCase();
         
-        let charDiv = `<div class="${classN}">
+        let charDiv = `<div class="char-container ${classN}">
+            <div class="bg ${classN}">
+                <img src="${n.bg}" />
+            </div>
+            <div class="gradient"></div>
             <div class="icon">
                 <img src="${n.icon}" />
             </div>
@@ -111,3 +116,26 @@ $.get('./js/characters.txt', function(data) {
         $('.char-scroll').append(charDiv);
     });
 }, 'text');
+
+let scrollObject = {
+    steven: 'translateX(0)',
+    garnet: 'translateX(-100%)',
+    amethyst: 'translateX(-200%)',
+    pearl: 'translateX(-300%)',
+    lapislazuli: 'translateX(-400%)',
+    peridot: 'translateX(-500%)',
+    jasper: 'translateX(-600%)'
+}
+
+$('.btn-scroll a').click(function() {
+    let element = $(this).attr('id');
+
+    $('.btn-scroll a').removeClass('active');
+    $(`#${element}`).addClass('active');
+
+    Object.keys(scrollObject).forEach(key => {
+        if (element == key) {
+            $('.char-container').css('transform', scrollObject[key]);
+        }
+    });
+});
